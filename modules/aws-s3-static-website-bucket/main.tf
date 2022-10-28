d89f88e4028d8245ac74a86a6ab87bd5f6edb33f
try-panwiac <93744932+try-panwiac@users.noreply.github.com>
2022-10-27 23:57:07 -0700

    Update main.tf

--- modules/aws-s3-static-website-bucket/main.tf
+++ modules/aws-s3-static-website-bucket/main.tf
@@ -3,11 +3,11 @@
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
 
-  tags = var.tags
+  tags = var.tags 
 }
 
 resource "aws_s3_bucket_website_configuration" "s3_bucket" {
-  bucket = aws_s3_bucket.s3_bucket.id
+  bucket = aws_s3_bucket.s3_bucket.id 
 
   index_document {
     suffix = "index.html"
@@ -21,11 +21,11 @@ resource "aws_s3_bucket_website_configuration" "s3_bucket" {
 resource "aws_s3_bucket_acl" "s3_bucket" {
   bucket = aws_s3_bucket.s3_bucket.id
 
-  acl = "public-read"
+  acl = "public-read" 
 }
 
 resource "aws_s3_bucket_policy" "s3_bucket" {
-  bucket = aws_s3_bucket.s3_bucket.id
+  bucket = aws_s3_bucket.s3_bucket.id 
 
   policy = jsonencode({
     Version = "2012-10-17"
